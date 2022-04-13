c3a0ef57132aa52eaa93adc0bf332c8cd58975c6
Adi Akbar <adi.akbar91@gmail.com>
2022-04-13 14:35:19 +0700

    connect to db mysql and auto migration

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"log"
+	"web-api-gin-golang/book"
 	"web-api-gin-golang/handler"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/web_api_gin_golang?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("DB connection error")
+	}
+
+	db.AutoMigrate(&book.Book{})
+
 	router := gin.Default()
 
 	v1 := router.Group("/v1")
